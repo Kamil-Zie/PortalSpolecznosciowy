88ad5f3032d5e7a785a94c44d6143775821b25b5
KacperPaszkowski <a@a.a>
2022-09-27 09:59:01 +0200

    auth context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './components/elements/styles/App.css';
 import Home from './components/home'
+import { AuthContext, AuthGuard } from './utils/auth'
 
 import {
   BrowserRouter as Router,
@@ -10,17 +11,20 @@ import {
 } from "react-router-dom";
 
 function App() {
+  const [context, setContext] = useState({auth: false, user: undefined});
+
   return (
-    <Router>
+    <AuthContext.Provider value={[context, setContext]}>
+      <Router>
            <div className="App">
            <Routes>
                  <Route exact path='/' element={<Home/>}></Route>
-                 <Route exact path='/about' element={<h1>DUPA1</h1>}></Route>
-                 <Route exact path='/contact' element={<h1>DUPA23 </h1>}></Route>
+                 <Route exact path='/contact' element={<AuthGuard><h1>DUPA1</h1></AuthGuard>}></Route>
+                 <Route exact path='/login' element={<h1>DUPA23 </h1>}></Route>
           </Routes>
           </div>
        </Router>
-    
+    </AuthContext.Provider>
   );
 }
 
