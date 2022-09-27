3ea7e2b129e4fecdf08a00fec0c373182938d6a1
Kamil <kamil6395@gmail.com>
2022-09-27 10:10:05 +0200

    fix

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -5,8 +5,6 @@ import {Link} from 'react-router-dom'
 function Home(){
     return(
         <div>
-            <Link to="/contact">Contact Us</Link>
-            <Link to="/">Home</Link>
             <LeftPanelSVG/>
             <RightPanel/>
         </div>
