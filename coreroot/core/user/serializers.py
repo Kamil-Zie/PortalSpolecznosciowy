101566cea378f37110fb9ac647cc32909f002a79
KacperPaszkowski <a@a.a>
2022-09-27 09:45:28 +0200

    rest fix

--- coreroot/core/user/serializers.py
+++ coreroot/core/user/serializers.py
@@ -7,4 +7,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'is_active']
-        read_only_field = ['is_active', 'created', 'updated']
+        read_only_field = ['is_active']
