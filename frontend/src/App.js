e1f390b23c7e789b621049c0525e17b6bac63ab5
Kamil <kamil6395@gmail.com>
2022-09-27 09:31:26 +0200

    Good react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './components/elements/styles/App.css';
+import Home from './components/home'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+           <div className="App">
+           <Routes>
+                 <Route exact path='/' element={<Home/>}></Route>
+                 <Route exact path='/about' element={<h1>DUPA1</h1>}></Route>
+                 <Route exact path='/contact' element={<h1>DUPA23 </h1>}></Route>
+          </Routes>
+          </div>
+       </Router>
+    
   );
 }
 
---
+++ frontend/src/components/elements/LeftPanelSVG.js
@@ -0,0 +1,12 @@
+import SVG from '../../asstes/panel.svg'
+import React from 'react';
+
+function LeftPanelSVG(){
+    return(
+        <div className="LeftPanelSVG">
+            <img src={SVG} alt='LeftPanelSVG'/>
+        </div>
+    );
+}
+
+export default LeftPanelSVG;
