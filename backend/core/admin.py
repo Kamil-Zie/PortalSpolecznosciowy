a113486a773e75ae65070a459b697f5cd3ef4c23
KacperPaszkowski <kacper.paszkowskee@gmail.com>
2022-11-08 09:09:27 +0100

    user register and login

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import S_User
+
+admin.site.register(S_User)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class S_User(models.Model):
+    mail = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
