bd5f454e32d8deb97c40de5e7691d684f014599b
Kamil Zieliński <85810595+RyyyyC@users.noreply.github.com>
2022-11-14 09:58:12 +0000

    file fix

--- assests/scripts/svgManipulating.js
+++
@@ -1,6 +0,0 @@
-const svg = document.getElementById("mainSVG");
-svg.setAttribute("viewBox", `0 0 ${window.innerWidth*0.8} ${window.innerHeight*0.8}`);
-
- window.addEventListener("resize", ()=>{
-     svg.setAttribute("viewBox", `0 0 ${window.innerWidth*0.8} ${window.innerHeight*0.8}`);
-})
