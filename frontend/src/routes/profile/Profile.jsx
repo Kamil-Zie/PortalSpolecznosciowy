786916f312a674de96d38fc887767e7bd0bfaf15
Kamil <kamil6395@gmail.com>
2022-12-12 12:35:02 +0100

    getstarted page

---
+++ frontend/src/routes/profile/Profile.jsx
@@ -0,0 +1,16 @@
+
+import styles from './css/profile.module.css'
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect, } from 'react';
+
+
+const Profile = () => {
+
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default Profile;
