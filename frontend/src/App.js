5cf9a6bf79469d9b6f96bb918760caa0c76c734f
Kamil <kamil6395@gmail.com>
2022-12-13 10:13:40 +0100

    post component getsrted dziala

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,21 @@ import Home from "./routes/home/Home";
 import SignInForm from "./components/login/pages/SignInForm";
 import SignUpForm from "./components/login/pages/SignUpForm";
 import GetStarted from "./routes/getstarted/GetStartedPage";
+import { MuiThemeProvider } from '@mui/material/styles';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-          <Route path="/*" element={<Home />}></Route>
-          <Route path="login/*" element={<Home />}>
-            <Route path="sign-in" element={<SignInForm/>}/>
-            <Route path="sign-up" element={<SignUpForm/>}/>
-          </Route>
-          <Route path="getstarted/" element={<GetStarted/>}></Route>
-      </Routes>
-    </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<Home />}></Route>
+            <Route path="login/*" element={<Home />}>
+              <Route path="sign-in" element={<SignInForm/>}/>
+              <Route path="sign-up" element={<SignUpForm/>}/>
+            </Route>
+            <Route path="getstarted/" element={<GetStarted/>}></Route>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- frontend/src/routes/getstarted/GetStartedPage.jsx
+++ frontend/src/routes/getstarted/GetStartedPage.jsx
@@ -1,32 +1,68 @@
 import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect, } from 'react';
 import styles from './css/getStarted.module.css';
+import { TextField ,Switch } from "@mui/material";
+import {createTheme} from '@mui/material/styles';
+import grey from '@mui/material/colors/grey';
+import Search from '@mui/icons-material/Search'
+import Post from '../../components/post/post.js'
+import InputAdornment from '@mui/material/InputAdornment';
+
 
 const GetStarted = () => {
+  const theme = createTheme({
+    palette:{
+      primary:{ main: grey[800] },
+      type: 'dark',
+    }
+  })
   return (
     <>
-    <div className={`${styles.changeTheme}`}>
-      <div className={`slider`}>
-        <label className={`${styles.switch}`}>
-          <input type="checkbox"/>
-          <span className={`${styles.slider} ${styles.round}`}></span>
-        </label>
+    <div className={`${styles.themeFriends}`}>
+      <div className={`${styles.changeTheme}`}>
+        <Switch theme={theme} />
+        <div className={`${styles.arrowPlacement}`}>
+          <div className={`${styles.arrowBlock}`}>
+            <div className={`${styles.themeArrowImage}`}/>
+          </div>
+          <div className={`${styles.changeThemeDesc}`}>
+            <p>Zmieniaj wygląd za pomocą <strong>Jednego Kliknięcia!</strong></p>
+          </div>
+        </div>
       </div>
-      <div className={`${styles.arrowPlacement}`}>
-        <div className={`${styles.arrowBlock}`}>
-          <div className={`${styles.themeArrowImage}`}/>
+      <div className={`${styles.newFriends}`}>
+        <div className={`${styles.newFriendsDesc}`}>
+          <p>Twórz <strong>Nowe Znajomości</strong>, a także znajduj interesujące Cię <strong>Grupy</strong>.</p>
         </div>
-        <div className={`${styles.changeThemeDesc}`}>
-          <p>Zmieniaj wygląd za pomocą <strong>Jednego Kliknięcia!</strong></p>
+        <div className={`${styles.newFriendsArrow}`}>
+
         </div>
+        <TextField
+        className={`${styles.newFriendsSampleSearch}`}
+        id="input-with-icon-textfield"
+        label="Znajdź znajomego"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <Search />
+            </InputAdornment>
+          ),
+        }}
+        variant="standard"
+        />
       </div>
     </div>
-    <div className={`${styles.posts}`}>
-      <div className={`${styles.postsDesc}`}>
-        <p>Przeglądaj <strong>Wpisy</strong> znajomych oraz ich <strong>Zdjęcia.</strong></p>
+    <div className={`${styles.postBlock}`}>
+      <div className={`${styles.posts}`}>
+        <div className={`${styles.postsDesc}`}>
+          <p>Przeglądaj <strong>Wpisy</strong> znajomych oraz ich <strong>Zdjęcia.</strong></p>
+        </div>
+        <div className={`${styles.arrowBlock}`}>
+          <div className={`${styles.postsArrowImage}`}></div>
+        </div>
       </div>
-      <div className={`${styles.arrowBlock}`}>
-        <div className={`${styles.postsArrowImage}`}></div>
+      <div className={`${styles.postsField}`}>
+        <Post/>
       </div>
     </div>
     </>
